9dff4770a64304a47386817d24c0de92fe559eb8
Ankan002 <56474413+Ankan002@users.noreply.github.com>
2022-03-24 12:46:37 +0530

    feat: :white_check_mark: Completed problem 3: Anagrams

---
+++ 02-Array-And-String/Problem03/anagrams.ts
@@ -0,0 +1,65 @@
+const anagrams = (s1: string, s2: string): boolean => {
+    if (s1.length !== s2.length) return false;
+
+    const alphabetMap: Map<string, number> = new Map();
+
+    let startingPoint: number = 0;
+    let endingPoint: number = s2.length - 1;
+
+    while(startingPoint <= endingPoint){
+        let currentCount: number | undefined = alphabetMap.get(s1.charAt(startingPoint));
+
+        if(currentCount === undefined) alphabetMap.set(s1.charAt(startingPoint), 1);
+        else alphabetMap.set(s1.charAt(startingPoint), currentCount + 1);
+
+        if(startingPoint === endingPoint){
+            startingPoint++;
+            endingPoint--;
+            continue;
+        }
+
+        currentCount = alphabetMap.get(s1.charAt(endingPoint));
+
+        if(currentCount === undefined) alphabetMap.set(s1.charAt(endingPoint), 1);
+        else alphabetMap.set(s1.charAt(endingPoint), currentCount + 1);
+
+        startingPoint++;
+        endingPoint--;
+    }
+
+    startingPoint = 0;
+    endingPoint = s2.length - 1;
+
+    while(startingPoint <= endingPoint){
+        let currentCount: number | undefined = alphabetMap.get(s2.charAt(startingPoint));
+
+        if(currentCount === undefined || currentCount <= 0) return false;
+        else alphabetMap.set(s2.charAt(startingPoint), currentCount - 1);
+
+        if(startingPoint === endingPoint){
+            startingPoint++;
+            endingPoint--;
+            continue;
+        }
+
+        currentCount = alphabetMap.get(s2.charAt(endingPoint));
+
+        if(currentCount === undefined || currentCount <= 0) return false;
+        else alphabetMap.set(s2.charAt(endingPoint), currentCount - 1);
+
+        startingPoint++;
+        endingPoint--;
+    }
+
+    return true;
+};
+
+console.log(anagrams('restful', 'fluster'));
+console.log(anagrams('cats', 'tocs'));
+console.log(anagrams('monkeyswrite', 'newyorktimes'));
+console.log(anagrams('paper', 'reapa'));
+console.log(anagrams('elbow', 'below'));
+console.log(anagrams('tax', 'taxi'));
+console.log(anagrams('taxi', 'tax'));
+console.log(anagrams('night', 'thing'));
+console.log(anagrams('abbc', 'aabc'));
